5989c128026495a53e927b25f987553141bbd0af
Chad McDaniel <ccm290@msstate.edu>
2022-03-09 10:17:46 -0600

    Add project files.

---
+++ Guessing Game App/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Guessing_Game_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(776, 93);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number Guessing Game";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(12, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(541, 42);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Guess a Number";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.BackColor = System.Drawing.Color.Black;
+            this.txtNumber.Font = new System.Drawing.Font("Ink Free", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumber.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtNumber.Location = new System.Drawing.Point(12, 194);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(541, 48);
+            this.txtNumber.TabIndex = 2;
+            this.txtNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(559, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 42);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Guess Log";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLog
+            // 
+            this.lblLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLog.BackColor = System.Drawing.Color.Black;
+            this.lblLog.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLog.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblLog.Location = new System.Drawing.Point(559, 194);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(229, 247);
+            this.lblLog.TabIndex = 4;
+            this.lblLog.Text = "<log>";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMessage.BackColor = System.Drawing.Color.White;
+            this.lblMessage.Font = new System.Drawing.Font("Ink Free", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMessage.ForeColor = System.Drawing.Color.Navy;
+            this.lblMessage.Location = new System.Drawing.Point(12, 352);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(541, 89);
+            this.lblMessage.TabIndex = 5;
+            this.lblMessage.Text = "<message>";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnGuess.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnGuess.Location = new System.Drawing.Point(129, 248);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(309, 101);
+            this.btnGuess.TabIndex = 6;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnGuess;
+    }
+}
+
---
+++ Guessing Game App/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guessing_Game_App
+{
+    public partial class Form1 : Form
+    {
+        
+        int com_number; //Store the computers number in the form.
+        
+        Random generator; //Create a generator object for selecting random numbers.
+
+        bool ongoing_game; //Store whether the user is currently playing the game.
+                           //used to determine whether the game should be reset when
+                           //clicking the button.
+
+        int guessed_num;   //Store the previous guess the user made.
+
+        double previous_distance; //Store the previous difference between the user's previous number.
+                                  //and the computers chosen number.
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Create the random generator object
+            generator = new Random();
+
+            //Reset the board on initialization to clear debug messages and to setup the game.
+            //Also used when the user starts another game.
+            restart();
+        }
+
+        private void restart()
+        {
+            //Setup the board by clearing it and choosing a new random number.
+            ongoing_game = true;
+            com_number = generator.Next(0, 1000);
+            lblMessage.Text = "";
+            lblLog.Text = "";
+            txtNumber.Text = "";
+            btnGuess.Text = "Guess";
+
+            //The starting distance is 1000 so that the users first number is 
+            //always "warm".
+            previous_distance = 1000;
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            //If a game is in session when this button is clicked, play the game.
+            if (ongoing_game == true)
+            {
+                int previous_guess = guessed_num;
+                try
+                {                   
+                    //Attempt to convert the user's number to a number.
+                    guessed_num = Convert.ToInt32(txtNumber.Text);
+                }
+                catch (FormatException error)
+                {
+                    //If it cannot be converted, the catch clause will trigger.   Set the error message
+                    //in the message box and immediately stop this functions execution.
+                    lblMessage.Text = "Error: This is not a valid number.  Enter another.";
+                    return;
+                }
+
+                //Add the user's number to the history log.
+                lblLog.Text = txtNumber.Text + "\n" + lblLog.Text;
+
+                //If the numbers match, set the textbox color to green and congradulate the user.
+                //Also, set the button text to "Restart" to indicate that pressing the button
+                //again will cause the game to restart.
+                if(guessed_num == com_number)
+                {
+                    txtNumber.BackColor = Color.Green;
+                    lblMessage.Text = "You got it correct!";
+                    btnGuess.Text = "Restart Game";
+                    ongoing_game = false;
+
+                }
+
+                //Otherwise, determine if the number is greater than/less than the computers number.
+                else if(guessed_num > com_number)
+                {
+                    //Determine the distance between the new number and the computers number.
+                    double new_distance = Math.Abs(com_number - guessed_num);
+
+                    //If the distance is shorter than the previous distance, then set the color
+                    //to red, indicating the user is getting "warmer" (i.e., closer to the
+                    //computer's chosen number).
+                    if (new_distance < previous_distance)
+                        txtNumber.BackColor = Color.Red;
+                    //If the distance is greater than the previous distance, then set the color
+                    //to blue, indicating the user is getting "colder" (i.e., farther from the
+                    //computer's chosen number).
+                    else if (new_distance > previous_distance)
+                        txtNumber.BackColor = Color.Blue;
+
+                    //If there is no change in distance, set the textbox's color to black.
+                    else
+                        txtNumber.BackColor = Color.Black;
+
+                    //Set the previous distance to the current distance for the next number.
+                    previous_distance = new_distance;
+                    
+                    //Indicate that the user's next number should be lower to help better guess
+                    //the new number.
+                    lblMessage.Text = "Incorrect.  Your next guess should be a lower number.";
+
+                }
+                else
+                {
+                    //Apply the same logic as before with distance.
+                    double new_distance = Math.Abs(com_number - guessed_num);
+
+                    if (new_distance < previous_distance)
+                        txtNumber.BackColor = Color.Red;
+                    else if (new_distance > previous_distance)
+                        txtNumber.BackColor = Color.Blue;
+                    else
+                        txtNumber.BackColor = Color.Black;
+
+                    previous_distance = new_distance;
+
+                    lblMessage.Text = "Incorrect.  Your next guess should be a higher number.";
+                }
+            }
+            //If the game is not in session, restart the game when this button is pressed.
+            else
+            {
+                restart();
+            }
+
+
+        }
+    }
+}
